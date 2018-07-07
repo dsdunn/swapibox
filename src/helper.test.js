eaf45afce234de0bb86609aef001b57eac2aa454
dsdunn <dsdunn@Davids-iMac.local>
2018-07-07 12:34:27 -0600

    Write helper tests

--- src/helper.test.js
+++ src/helper.test.js
@@ -10,71 +10,128 @@ import {
   getHomeWorld, 
   grabScroll} from './helper';
 
-const mockScroll = "this is the crawl for a movie";
-let url = "https://swapi.co/api/films";
-let url2 = "https://swapi.co/api/planets/1/";
-const mockFilmInfo = {
-  filmTitle: "Cowabunga",
-  date: "1977-05-25",
-  crawl: mockScroll
-}
-
-window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
-  json: () => Promise.resolve({
-    count : 0,
-    results: [{title: "Cowabunga",
-              release_date: "1977-05-25",
-              opening_crawl: mockScroll}]
-  })
-}))
 
 describe('helper', () => {
 
   it('calls fetch with the right data when grabScroll is called', () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve({
+        count : 0,
+        results: [{title: "Cowabunga",
+                  release_date: "1977-05-25",
+                  opening_crawl: ''}]
+      })
+    }))
+
+    const filmsURL = "https://swapi.co/api/films";
+
     grabScroll();
 
-    expect(window.fetch).toHaveBeenCalledWith(url);
+    expect(window.fetch).toHaveBeenCalledWith(filmsURL);
   })
 
-  it('returns mockScroll when grabScroll is called', async () => {
+  it('returns mockFilmInfo when grabScroll is called', async () => {
+    const mockFilmInfo = {
+      filmTitle: "Cowabunga",
+      date: "1977-05-25",
+      crawl: ''
+    }
+
     const actual = await grabScroll();
     expect(actual).toEqual(mockFilmInfo);
   })
 
   it('calls fetch with the right data when grabPeople is called', () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve)
 
-    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
-      json: () => Promise.resolve({
-        count : 0,
-        results: [{name: "Chucky",
-                  homeworld: url2,
-                  species: ['bullhockey']}]
-      })
-    }))
-
-    url = 'https://swapi.co/api/people';
+    const peopleURL = 'https://swapi.co/api/people';
 
     grabPeople();
 
-    expect(window.fetch).toHaveBeenCalledWith(url);
+    expect(window.fetch).toHaveBeenCalledWith(peopleURL);
 
   })
 
-  it('calls getHomeWorld when grabPeople is called', async () => {
-    const getHomeWorld = jest.fn().mockImplementation(() => {
-      Promise.resolve({
-        json: () => Promise.resolve({
-          planetName: "Bumfuck",
-          population: "1"
-        })
-      })
-    })
+  it.skip('should return mockPeople when grabPeople is called', async () => {
+    const mockPeople = [{
+      name: "Chucky",
+      homeworld: 'Earth',
+      species: ['ginger']
+    }]
+
+    let actual = await grabPeople()
+    // getHomeWorld = jest.fn(()=> Promise.resolve({
+    //   json: () => Promise.resolve({
+    //     count : 0,
+    //     results: [{name: "Earth",
+    //               population: '7 billion',
+    //               }]
+    //   })
+    // }))
+    expect(actual).toEqual(mockPeople)
+  })
+
+  it.skip('calls getHomeWorld when grabPeople is called', async () => {
+    const mockGetHomeWorld = jest.fn(() => Promise.resolve)
+
+    // let spy = jest.spyOn(helper, 'getHomeWorld').mockImplementation(()=> mockGetHomeWorld)
+
     await grabPeople();
 
-    expect(getHomeWorld).toHaveBeenCalled();
+    expect().toHaveBeenCalled();
   })
 
+  it.skip('calls getSpecies when grabPeople is called', async () => {
 
+  })
 
+  it.skip('returns mockHomeWorld when getHomeWorld is called', () => {
+    const mockHomeWorld = { planetNmae: 'Earth', population: '7 billion' }
+  })
+
+  it.skip('returns mockSpeciesName when getSpecies is called', () => {
+    const mockSpeciesName = 'human';
+  })
+
+  it('calls fetch with the right data when grabVehicles is called', () => {
+    const vehicleURL = 'https://swapi.co/api/vehicles/'
+
+    grabVehicles();
+
+    expect(window.fetch).toHaveBeenCalledWith(vehicleURL);
+  })
+
+  it('returns mockVehicles when grabVehicles is called', async () => {
+    const mockVehicles = [{
+      name:'mustang',
+      model: '4.0',
+      vehicle_class: 'muscle car',
+      passengers: '4'
+    }]
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve({
+        count : 0,
+        results: mockVehicles
+      })
+    }))
+
+    let actual = await grabVehicles();
+
+    expect(actual).toEqual(mockVehicles)
+  })
+
+  it('calls fetch with the right data when grabPlanets is called', () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve)
+
+    const planetsURL = 'https://swapi.co/api/planets';
+
+    grabPlanets();
+
+    expect(window.fetch).toHaveBeenCalledWith(planetsURL);
+  })
+
+  it.skip('returns mockResidentNames when getResidents is called', () => {
+    const mockResidentNames = ['Jules Verne', 'Sam Jackson']
+  })
 
 })
