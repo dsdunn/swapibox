db9472bb52f1bbfa19387d4d1de6eabf3613174a
dsdunn <dsdunnmusic@gmail.com>
2018-08-06 12:57:26 -0600

    import style to index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App';
 import App from './App';
+import './index.css'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
